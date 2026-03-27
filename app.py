70c7e21caf4c4d73111753dd4ab25665ba8b39e6
urkia96 <asurquia@ucm.es>
2026-03-27 13:56:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ c = t["cols"]
 # --- 2. CARGA DE RECURSOS (Definición y Ejecución) ---
 @st.cache_resource
 def load_resources():
-    excel_path = f"{PATH_RECO}/CATALOGO_PROCESADO_version3.xlsx"
+    excel_path = f"{PATH_RECO}/metadadatos_clubes_lectura.xlsx"
     disp_path = f"{PATH_RECO}/disponibilidad_catalogo_completo.xlsx" # Ruta disponibilidad
 
     if not os.path.exists(excel_path):
@@ -258,11 +258,11 @@ def load_resources():
     df['autor_norm'] = df['Autor'].apply(normalizar_texto)
     
     # Carga de Metadatos IA (Tu código exacto)
-    with open(f"{PATH_RECO}/metadatos_promptss_infloat_ponderado_small.pkl", "rb") as f:
+    with open(f"{PATH_RECO}/metadatos_cl_small.pkl", "rb") as f:
         df_ia_meta = pickle.load(f)
     df_ia_meta['Nº lote'] = df_ia_meta['Nº lote'].astype(str).str.strip()
     
-    index = faiss.read_index(f"{PATH_RECO}/biblioteca_prompts_infloat_ponderado_small.index")
+    index = faiss.read_index(f"{PATH_RECO}/metadatos_cl_small.index")
     model = SentenceTransformer('intfloat/multilingual-e5-small')
     
     gc.collect()
