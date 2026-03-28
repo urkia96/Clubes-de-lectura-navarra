7e8d73b698f0f7c84c36ffb965f8eed341445225
urkia96 <asurquia@ucm.es>
2026-03-28 15:39:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -309,12 +309,12 @@ def load_resources():
     df['autor_norm'] = df['Autor'].apply(normalizar_texto)
     
     # Carga de Metadatos IA (CORREGIDO)
-    with open(f"{PATH_RECO}/metadatos_cl_small_v3.pkl", "rb") as f:
+    with open(f"{PATH_RECO}/metadatos_cl_small_v4.pkl", "rb") as f:
         data = pickle.load(f)
     
     metadata = data["metadata"]
     
-    index = faiss.read_index(f"{PATH_RECO}/metadatos_cl_small_v3.index")
+    index = faiss.read_index(f"{PATH_RECO}/metadatos_cl_small_v4.index")
     model = SentenceTransformer('intfloat/multilingual-e5-small')
     
     #df = data["df"]
