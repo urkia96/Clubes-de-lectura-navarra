f1168967f1fa99d9d40f4cceebc92d7f2c196239
urkia96 <asurquia@ucm.es>
2026-03-29 16:18:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -263,8 +263,8 @@ def load_resources():
     # 1. Rutas de archivos
     excel_path = f"{PATH_RECO}/metadatos_entidades_OA.xlsx"
     disp_path = f"{PATH_RECO}/disponibilidad_catalogo_completo.xlsx"
-    ruta_pkl = f"{PATH_RECO}/clubes_lectura_small_v20.pkl"
-    ruta_index = f"{PATH_RECO}/clubes_lectura_small_v20.index"
+    ruta_pkl = f"{PATH_RECO}/clubes_lectura_small_v21.pkl"
+    ruta_index = f"{PATH_RECO}/clubes_lectura_small_v21.index"
 
     # Verificar existencia del Excel principal
     if not os.path.exists(excel_path):
